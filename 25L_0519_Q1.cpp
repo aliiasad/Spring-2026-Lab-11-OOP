afcd0d4c1c1e99d3e5a2c1b2b4849a323424a046
aliiasad <alii.asad0519@gmail.com>
2026-04-17 10:32:47 +0500

    Derived Class Level 2: GymTrainer

--- 25L_0519_Q1.cpp
+++ 25L_0519_Q1.cpp
@@ -30,3 +30,34 @@ void Trainer :: showInfo()  {
     cout << "Trainer Name: " << name << " | Trainer Age: " << age << endl;
     return;
 }
+
+// derived class on level 2
+class GymTrainer : public Trainer {
+    protected:
+        string gymName;
+        string specialtyType;
+    public:
+        // setters
+        void setGym(string);
+        void setType(string);
+
+        // helper
+        void showInfo();
+};
+
+void GymTrainer :: setGym(string gymName)   {
+    this->gymName = gymName;
+    return;
+}
+
+void GymTrainer :: setType(string specialtyType)    {
+    this->specialtyType = specialtyType;
+    return;
+}
+
+void GymTrainer :: showInfo() {
+    Trainer :: showInfo(); // call parent's showInfo, method overriding
+    cout << "Gym: " << gymName << endl;
+    cout << "Type: " << specialtyType << endl;
+    return;
+}
